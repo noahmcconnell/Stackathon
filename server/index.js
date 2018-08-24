01fd3e2f4f6207e615715e305a52d745761888c7
Keith Root <hazelroot11@gmail.com>
2018-08-24 13:27:21 -0600

    Set up index

--- server/index.js
+++ server/index.js
@@ -4,6 +4,22 @@ require('./db/db-config')
 let server = express();
 let port = 3000
 
+server.use(bp.json())
+server.use(bp.urlencoded(({
+    extended:true
+})))
+server.use(express.static(__dirname + '/../www/'))
+
 //ROUTES
+let answerRoutes = require('./routes/answers')
+let postRoutes = require('./routes/posts')
+let usersRoutes = require('./routes/users')
+
 
 //ERROR Handlers
+server.use('/api/*', (err, req, res, next) => {
+    res.status(400).send(err)
+})
+server.unsubscribe('*', (req, res, next) => {
+    res.status(404).send('<h1>404 Error<h1>')
+})
