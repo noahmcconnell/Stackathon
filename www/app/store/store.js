a358c15660dc1d1a4ea41b2ee40ce45110a1118c
matt <matthew.dobrowolski5@gmail.com>
2018-08-24 20:08:38 -0600

    store!!!

---
+++ www/app/store/store.js
@@ -0,0 +1,64 @@
+import User from "../models/user.js";
+import Goal from "../models/goal.js";
+import Post from "../models/post.js";
+
+
+let store
+
+let state = {
+    user: {},
+    post: {},
+    
+    
+    
+}
+function setState(prop, data){
+    state[prop] = data
+    console.log(state)
+}
+
+export default class Store {
+    getPost(draw){
+        fetch('/api/post/by-user/'+ state.user._id)
+        .then(res=> res.json())
+        .then(data=>{
+            setState('post',data.map(post=> new Post(post)))
+            draw()
+        })
+
+    }
+    login(creds, draw){
+        fetch('/auth/login',{
+        method:'post',
+        body: JSON.stringify(creds),
+        headers:{
+            "Content-Type":"application/json; charset=utf-8"
+        },
+    })
+    .then(res => res.json())
+    .then(data => {
+        setState('user', new User(data))
+        draw()
+    })
+    .catch(console.error)
+}
+
+
+
+
+
+    
+    
+    constructor() {
+        if (store) {
+            return store
+        }
+        store = this
+    }
+    get state() {
+        return {
+            ...state
+        }
+    }
+    
+}
