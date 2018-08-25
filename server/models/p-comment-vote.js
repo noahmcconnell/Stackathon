569c935ac96c03c5bcba20f0ad7e15f231808019
Keith Root <hazelroot11@gmail.com>
2018-08-25 02:22:34 -0600

    Vote Models

---
+++ server/models/p-comment-vote.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const schema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true
+      },
+      post: {
+        type: Schema.Types.ObjectId,
+        required: true
+      },
+      direction: {
+        type: Boolean,
+        required: true
+      }
+});
+
+module.exports = mongoose.model('pCommentVote', schema);
