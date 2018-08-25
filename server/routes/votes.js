e0034779b97b3e8ff5ac8c9e5a5d633f182687ab
Keith Root <hazelroot11@gmail.com>
2018-08-25 02:52:23 -0600

    Router link edit

--- server/routes/votes.js
+++ server/routes/votes.js
@@ -5,7 +5,7 @@ const AnswerVote = require('../models/answer-vote.js');
 const ACommentVote = require('../models/a-comment-vote.js');
 
 
-router.put('/:id', (req, res, next) =>
+router.put('/post/:postId', (req, res, next) =>
   PostVote.findByIdAndUpdate(req.params.id, req.body, {
       upsert: true
   })
@@ -13,13 +13,13 @@ router.put('/:id', (req, res, next) =>
     .catch(next)
 );
 
-router.delete('/:id', (req, res, next) =>
+router.delete('/post/:postId', (req, res, next) =>
   PostVote.findByIdAndRemove(req.params.id)
     .then(() => res.send({ message: 'Successfully deleted item.' }))
     .catch(next)
 );
 
-router.put('/:id', (req, res, next) =>
+router.put('/comments/post/:postId', (req, res, next) =>
   PCommentVote.findByIdAndUpdate(req.params.id, req.body, {
       upsert: true
   })
@@ -27,13 +27,13 @@ router.put('/:id', (req, res, next) =>
     .catch(next)
 );
 
-router.delete('/:id', (req, res, next) =>
+router.delete('/comments/post/:postId', (req, res, next) =>
   PCommentVote.findByIdAndRemove(req.params.id)
     .then(() => res.send({ message: 'Successfully deleted item.' }))
     .catch(next)
 );
 
-router.put('/:id', (req, res, next) =>
+router.put('answer/:postId', (req, res, next) =>
   AnswerVote.findByIdAndUpdate(req.params.id, req.body, {
       upsert: true
   })
@@ -41,13 +41,13 @@ router.put('/:id', (req, res, next) =>
     .catch(next)
 );
 
-router.delete('/:id', (req, res, next) =>
+router.delete('answer/:postId', (req, res, next) =>
   AnswerVote.findByIdAndRemove(req.params.id)
     .then(() => res.send({ message: 'Successfully deleted item.' }))
     .catch(next)
 );
 
-router.put('/:id', (req, res, next) =>
+router.put('/comment/answer/:postId', (req, res, next) =>
   ACommentVote.findByIdAndUpdate(req.params.id, req.body, {
       upsert: true
   })
@@ -55,7 +55,7 @@ router.put('/:id', (req, res, next) =>
     .catch(next)
 );
 
-router.delete('/:id', (req, res, next) =>
+router.delete('/comment/answer/:postId', (req, res, next) =>
   ACommentVote.findByIdAndRemove(req.params.id)
     .then(() => res.send({ message: 'Successfully deleted item.' }))
     .catch(next)
