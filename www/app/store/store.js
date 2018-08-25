24d0604152f2b28b94d02a5ad3c3c7288cfc3f0c
matt <matthew.dobrowolski5@gmail.com>
2018-08-24 22:14:25 -0600

    stuff

--- www/app/store/store.js
+++ www/app/store/store.js
@@ -1,72 +1,53 @@
 import User from "../models/user.js";
 import Post from "../models/post.js";
 
-
-
-
-let store
+let store;
 
 let state = {
-    user:{},
-    currentPost: {},
-
-
-    
-    
-    
-    
-}
-function setState(prop, data){
-    state[prop] = data
-    console.log(state)
+  user: {},
+  currentPost: {}
+};
+function setState(prop, data) {
+  state[prop] = data;
+  console.log(state);
 }
 
 export default class Store {
-    getPost(id){
+  getPost(id) {
+    return fetch("/api/post/by-id/" + id)
+      .then(res => res.json())
+      .then(data => {
+        setState("post", data.map(post => new Post(post)));
+      });
+  }
 
-        return fetch('/api/post/by-id/'+ id)
-        .then(res=> res.json())
-        .then(data=>{
-            setState('post',data.map(post=> new Post(post)))
-            
-        })
-    }
-            
-
-    login(creds){
-        return fetch('/auth/login',{
-        method:'post',
-        body: JSON.stringify(creds),
-        headers:{
-            "Content-Type":"application/json; charset=utf-8"
-        },
+  login(creds) {
+    return fetch("/auth/login", {
+      method: "post",
+      body: JSON.stringify(creds),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
     })
-    .then(res => res.json())
-    .then(data => {
-        setState('user', new User(data))
-    })
-    .catch(error=>console.error(error)
-        
-    
-    )
-}
+      .then(res => res.json())
+      .then(data => {
+        if (data.error) return error;
+        setState("user", new User(data));
+      })
+      .catch(error => console.error(error));
+  }
 
+  //dis dat SINGLETON
 
-
-
-//dis dat SINGLETON
-    
-    
-    constructor() {
-        if (store) {
-            return store
-        }
-        store = this
+  constructor() {
+    if (store) {
+      return store;
     }
-    get state() {
-        return {
-            ...state
-        }
-    }
-    
+    store = this;
+  }
+  get state() {
+    return {
+      ...state
+    };
+  }
 }
