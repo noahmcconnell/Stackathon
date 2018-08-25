7efcb54fbd569dcb53324c9a488e69f4d1d5a3c9
Christopher Brown <chbphone55@gmail.com>
2018-08-24 19:45:14 -0600

    add new controller, move controllers out of individual folders, finished individual post mockup

---
+++ www/app/components/post-controller.js
@@ -0,0 +1,47 @@
+import Store from '../../store/store.js';
+const store = new Store();
+
+const main = document.getElementById('main');
+
+function draw() {
+  main.innerHTML = `
+    <section class='post container'>
+      <header class='post-header flex'>
+        <i class='material-icons favorite'>star</i>
+        <h3 class='title'>Overlap columns in bootstrap 4</h3>
+        <div class='upvote-downvote flex align-items-center'>
+          <i class='material-icons'>thumb_up_alt</i>
+          <span>0</span>
+          <i class='material-icons'>thumb_down_alt</i>
+        </div>
+      </header>
+      <article class='article'>
+        <a href='#'>https://atom.io/packages/data-atom</a>
+        <p>I have a MySQL connection through SSH that I have configured on Sequel Pro but like the idea of using this Atom
+          package, data-atom, instead. My issue is that I cannot seem to figure out the js behind it to determine if it
+          supports an SSH connection and if so how I can accomplish it with the configuration.</p>
+        <button class='btn-icon'>edit <i class='material-icons'>edit</i></button>
+        <div class='comments flex flex-column'>
+          <article class="comment flex">
+            <div class='flex flex-column upvote-downvote vertical'>
+              <i class='material-icons'>thumb_up_alt</i>
+              <span align='center'>0</span>
+              <i class='material-icons'>thumb_down_alt</i>
+            </div>
+            <p>You will notice that the basic controllers and html structure has been setup for you. You will be responsible
+              to connect each of the controllers to their appropiate service. Every Service is a class so be sure to invoke
+              the service inside the controller and save it to a variable in the controller to gain access to the needed
+              methods. Take a look at the weather controller for an example of newing up the services. Also don't forget
+              to instantiate your controllers and add them to the app object.<span class="user-info">– <span class='blue-text'>Username</span>
+                3 hrs ago</p>
+          </article>
+        </div>
+      </article>
+      <button class='btn-icon mt-1'>add comment <i class='material-icons'>add_comment</i></button>
+    </section>
+  `;
+}
+
+export default class PostController {
+  constructor() {}
+}
